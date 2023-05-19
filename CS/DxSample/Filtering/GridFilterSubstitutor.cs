73c2c1447a8d83ba4c7087a20b3b9aaf75e19117
AlexanderDevX <118719960+AlexanderDevX@users.noreply.github.com>
2023-05-19 13:41:05 +0400

    Updated to utilize ClientCriteriaLazyPatcherBase.AggregatesCommonProcessingBase as a base class

--- CS/DxSample/Filtering/GridFilterSubstitutor.cs
+++ CS/DxSample/Filtering/GridFilterSubstitutor.cs
@@ -1,24 +1,26 @@
 ﻿using DevExpress.Data.Filtering;
+using DevExpress.Data.Filtering.Helpers;
 
 namespace DxSample.Filtering {
-    public class GridFilterSubstitutor :ClientCriteriaVisitorBase {
+    public class GridFilterSubstitutor : ClientCriteriaLazyPatcherBase.AggregatesCommonProcessingBase
+    {
         private static CriteriaOperator WrapIntoCustomFunction(CriteriaOperator param) {
             return new FunctionOperator(FunctionOperatorType.Custom, 
                 new ConstantValue("RemoveDiacritics"), (CriteriaOperator)param);
         }
 
         public static CriteriaOperator Substitute(CriteriaOperator source) {
-            return new GridFilterSubstitutor().AcceptOperator(source);
+            return new GridFilterSubstitutor().Process(source);
         }
 
-        protected override CriteriaOperator VisitFunction(FunctionOperator theOperator) {
+        public override CriteriaOperator Visit(FunctionOperator theOperator) {
             if (theOperator.OperatorType == FunctionOperatorType.StartsWith ||
                 theOperator.OperatorType == FunctionOperatorType.EndsWith ||
                 theOperator.OperatorType == FunctionOperatorType.Contains)
                 return new FunctionOperator(theOperator.OperatorType,
-                    GridFilterSubstitutor.WrapIntoCustomFunction(theOperator.Operands[0]),
-                    GridFilterSubstitutor.WrapIntoCustomFunction(theOperator.Operands[1]));
-            return base.VisitFunction(theOperator);
+                    WrapIntoCustomFunction(theOperator.Operands[0]),
+                    WrapIntoCustomFunction(theOperator.Operands[1]));
+            return base.Visit(theOperator);
         }
     }
 }
